96151f1df42422bb5700700417a291dff98fb84a
nursyahrina <nursyahrina17@gmail.com>
2022-07-08 20:02:29 +0700

    modifying navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav)
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#3f352a] text-[#f3973b] drop-shadow-lg'>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#342c34] text-[#f3973b] drop-shadow-lg'>
       <div>
         <img src={Logo} alt="Logo" className='w-[50px] rounded-xl'/>
       </div>
@@ -31,33 +31,33 @@ const Navbar = () => {
       </div>
 
       {/* Mobile menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#3f352a] text-[#f3973b] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>About</li>
-        <li className='py-6 text-4xl'>Skills</li>
-        <li className='py-6 text-4xl'>Projects</li>
-        <li className='py-6 text-4xl'>Contacts</li>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#342c34] text-[#f3973b] flex flex-col justify-center items-center'}>
+        <li className='py-6 text-3xl'>Home</li>
+        <li className='py-6 text-3xl'>About</li>
+        <li className='py-6 text-3xl'>Skills</li>
+        <li className='py-6 text-3xl'>Projects</li>
+        <li className='py-6 text-3xl'>Contacts</li>
       </ul>
       {/* Social icons */}
-      <div className='fixed flex flex-col top-1/4 left-0'>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5]'>
-            <a className='flex justify-between items-center w-full text-white' 
+          <li className='rounded-tr-xl hover:rounded-r-xl w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077b5] drop-shadow-2xl'>
+            <a className='flex justify-between items-center w-full text-[#fffffc]' 
             href="/">Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#24292f]'>
-            <a className='flex justify-between items-center w-full text-white' 
+          <li className='hover:rounded-r-xl w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#24292f] drop-shadow-2xl'>
+            <a className='flex justify-between items-center w-full text-[#fffffc]' 
             href="/">Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#bb001b]'>
-            <a className='flex justify-between items-center w-full text-white' 
+          <li className='hover:rounded-r-xl w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#bb001b] drop-shadow-2xl'>
+            <a className='flex justify-between items-center w-full text-[#fffffc]' 
             href="/">Email <HiOutlineMail size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#c0660c]'>
-            <a className='flex justify-between items-center w-full text-white' 
+          <li className='rounded-br-xl hover:rounded-r-xl w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#c0660c] drop-shadow-2xl'>
+            <a className='flex justify-between items-center w-full text-[#fffffc]' 
             href="/">Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
