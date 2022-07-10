aec44fb93b82e733423db513818cb79107806aa5
nursyahrina <nursyahrina17@gmail.com>
2022-07-10 11:47:52 +0700

    add typewriting animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import TypeAnimation from 'react-type-animation';
 
 const Home = () => {
   return (
@@ -7,9 +8,15 @@ const Home = () => {
       
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='mb-2 text-l text-[#f3973b]'>Hi everyone! It's me,</p>
+        <TypeAnimation className='mb-2 text-xl text-[#f3973b]'
+          cursor={false}
+          sequence={["Hi everyone! 👋 It's me,", 1000, '']}
+          wrapper="p"
+          repeat={Infinity}
+        />
         <h1 className='text-5xl sm:text-7xl font-semibold text-[#e3dee3]'>Nursyahrina</h1>
         <h2 className='text-4xl sm:text-6xl font-semibold text-[#bdb2bd]'>I'm a Web Developer.</h2>
+        
         <p className='max-w-[700px] py-4 text-l text-[#bdb2bd] '>I'm a final year student in Informatics Engineering transitioning into a professional software developer. Currently, I'm focusing on building web applications.</p>
         <div>
           <button className='group text-[#f6f4f6] text-sm font-bold border-2 rounded-xl px-6 py-3 my-2 flex items-center hover:bg-[#f3973b] hover:border-[#f3973b]'>View Projects
